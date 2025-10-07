f07ef9903a975bc696f1c2f5e22082594a9e1cb2
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-10-07 10:38:19 +0530

    Update app.json to change windowSoftInputMode to softwareKeyboardLayoutMode; add PreviewIndex component for document preview

---
+++ app/(preview)/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function PreviewIndex() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Please select a document to preview.</Text>
+    </View>
+  );
+}
